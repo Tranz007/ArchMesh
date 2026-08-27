667485a882e7bf70aaefe908caf88cce9c27476f
Tony Moura <tmoura@me.com>
2026-08-27 19:23:37 -0400

    style: load 3D graph controls

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './inspector.css';
 import './viewport.css';
+import './graph-3d.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
